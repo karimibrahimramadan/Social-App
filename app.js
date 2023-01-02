ce0de300437c329b8edb20b23c78375ca6fcaa82
karimibrahimramadan <karimibrahimramadan@gmail.com>
2023-01-02 11:38:39 +0200

    Security improvements

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ const helmet = require("helmet");
 const routesController = require("./routes/routesController");
 const errorHandler = require("./controllers/errorController");
 const path = require("path");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+const xss = require("xss-clean");
 
 const app = express();
+const limiter = rateLimiter({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  legaceHeaders: false,
+  standardHeaders: true,
+  message: "Too many requests from this IP. Please try again in an hour!",
+});
 
 // middlewares
 app.use(helmet());
@@ -17,6 +28,10 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use("/api/v1/uploads", express.static(path.join(__dirname, "./uploads")));
+app.use("/api/v1", limiter);
+app.use(xss());
+app.use(hpp());
+app.use(mongoSanitize());
 
 // routes
 app.use("/api/v1/comments", routesController.commentRouter);
